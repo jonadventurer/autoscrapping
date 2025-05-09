928eed2695c1ec49e220e6ac6d067f238e68adb3
Fardin Farhan Ahmed <fardin5215@gmail.com>
2025-05-09 10:22:38 +0800

    Gabo Island Council

--- mycommunitydirectory.py
+++ mycommunitydirectory.py
@@ -32,7 +32,7 @@ creds_dict = json.loads(creds_json)
 creds      = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, SCOPE)
 client     = gspread.authorize(creds)
 
-COUNCIL_NAME       = "Casey Council"
+COUNCIL_NAME       = "Gabo Island Council"
 SHEET_NAME         = "VICTORIA Australian Council [MyCommunity] Scrapping - Tracking"
 TRACKING_SHEET     = "Tracking Code (0 results)"
 OUTPUT_NAME        = "My Community Scrapping (Victoria) state - By Council Tabs"
